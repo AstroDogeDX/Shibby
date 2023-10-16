8a3c1a0fab82b123f58896f8931f83339a372eba
AstroDogeDX <48888500+AstroDogeDX@users.noreply.github.com>
2023-10-16 18:21:20 +0100

    Modified intents to support voice

--- bot.js
+++ bot.js
@@ -6,7 +6,8 @@ const getFiles = require('./lib/getFiles.js');
 const client = new Discord.Client({
     intents: [
         Discord.Intents.FLAGS.GUILDS,
-        Discord.Intents.FLAGS.GUILD_MESSAGES
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_VOICE_STATES
     ]
 });
 
