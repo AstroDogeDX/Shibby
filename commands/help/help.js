3fa3c4ec9a5a400b786fa6bf790fcedaf2fca736
AstroDogeDX <48888500+AstroDogeDX@users.noreply.github.com>
2024-04-24 14:17:14 +0100

    Updated to point at RM because Discord char limit

--- commands/help/help.js
+++ commands/help/help.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
-const bothelp = fs.readFileSync('./README.md', 'utf8');
 
 module.exports = {
     name: '!help',
     execute: (message) => {
-        message.author.send(bothelp);
+        message.author.send(`You can find comprehensive and up-to-date documentation on the bot's GitHub page: https://github.com/AstroDogeDX/Shibby/blob/main/README.md`);
         message.delete().catch(error => console.error(`Couldn't delete original command message because of: ${error}`));
     }
 };
