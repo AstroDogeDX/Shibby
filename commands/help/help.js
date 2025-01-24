0f9f074d19eedf95d8d5f254c9865303f518bceb
AstroDogeDX <48888500+AstroDogeDX@users.noreply.github.com>
2025-01-24 11:12:33 +0000

    Changed !help to just post the readme link in a channel since DM settings often block Shibby

--- commands/help/help.js
+++ commands/help/help.js
@@ -2,8 +2,13 @@ const fs = require('fs');
 
 module.exports = {
     name: '!help',
-    execute: (message) => {
-        message.author.send(`You can find comprehensive and up-to-date documentation on the bot's GitHub page: https://github.com/AstroDogeDX/Shibby/blob/main/README.md`);
-        message.delete().catch(error => console.error(`Couldn't delete original command message because of: ${error}`));
+    execute: async (message) => {
+        const helpMessage = await message.channel.send(`Hi ${message.author}, you can find all of my commands here: https://github.com/AstroDogeDX/Shibby/blob/main/README.md\n-# This message will auto-delete in 10 seconds`);
+        
+        // Delete both messages after 10 seconds
+        setTimeout(() => {
+            helpMessage.delete().catch(error => console.error(`Couldn't delete help message because of: ${error}`));
+            message.delete().catch(error => console.error(`Couldn't delete original command message because of: ${error}`));
+        }, 10000);
     }
 };
